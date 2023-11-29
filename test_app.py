136fa4e72fcad2df6ef2e774d4aa1751fb9fd56c
Stanley Ndagi <ndagis@gmail.com>
2023-11-29 14:36:48 +0300

    update links in scripts

--- test_app.py
+++ test_app.py
@@ -2,13 +2,16 @@ from app import app
 
 import unittest
 
+# From CircleCI's CIRCLECI-GWP/pytorch-flask-model's repository
+cat_image_url = "https://raw.githubusercontent.com/CIRCLECI-GWP/pytorch-flask-model/06d0d0ea0d05dd9792e93a9db63678797c018d4a/test_images/cat_image.jpeg"
+
 class TestPredictionsAPI(unittest.TestCase):
     def test_cat_image_response(self):
         self.app = app
         self.client = self.app.test_client
 
         request = {
-            'image_url': 'https://i.imgur.com/t5S6rkz.jpg'
+            'image_url': cat_image_url
         }
 
         response = self.client().post('/predict', data=request)
@@ -20,7 +23,7 @@ class TestPredictionsAPI(unittest.TestCase):
         self.client = self.app.test_client
 
         request = {
-            'image_url': 'https://i.imgur.com/THIS_IS_A_BAD_URL.jpg'
+            'image_url': 'https://github.com/CIRCLECI-GWP/THIS_IS_A_BAD_URL.jpg'
         }
 
         response = self.client().post('/predict', data=request)
